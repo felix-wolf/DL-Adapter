bd2614cda9fae5b3b837e1cb98d8a4ed26d466f2
Felix Wolf <x3mdroid@gmail.com>
2021-06-23 19:24:22 +0200

    fill database with 5000 rows

--- src/main/java/core/DatabasePreparer.java
+++ src/main/java/core/DatabasePreparer.java
@@ -5,7 +5,7 @@ import java.sql.Timestamp;
 public class DatabasePreparer {
 
     static void fillDatabase() {
-        for (int i = 0; i < 50000; i++) {
+        for (int i = 0; i < 5000; i++) {
             Timestamp timestamp = new Timestamp(System.currentTimeMillis() + 1000000);
             new DatabaseRequester().execAction("INSERT INTO BOOK(id, title, author, publisher, isavail, updated_at) VALUES('" + i + "', 'test', 'test', 'test', true, '"+ timestamp + "')");
         }
