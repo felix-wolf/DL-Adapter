eaf45bdff10ee9b5ddf904f83dca88e01a75a011
Felix Wolf <x3mdroid@gmail.com>
2021-06-23 19:23:56 +0200

    create environment to do performance tests in

--- src/main/java/core/DatabaseRequester.java
+++ src/main/java/core/DatabaseRequester.java
@@ -1,28 +1,24 @@
 package core;
 
-import helper.Utils;
-import models.ObjectType;
-import models.Operation;
-import models.OperationType;
-import models.extended.*;
+import models.extended.BookExtended;
 import timer.TimerUtil;
 
 import javax.swing.*;
 import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Objects;
 
 public class DatabaseRequester {
 
-    private Date lastRead = null;
     private static final String DB_URL_LIVE = "jdbc:derby://localhost:1527/database;create=true";
     private static final String DRIVER_LIVE = "org.apache.derby.jdbc.EmbeddedDriver";
     private static Connection conn = null;
 
     public DatabaseRequester() {
         createConnection();
-        initialiseTimer();
     }
 
     /**
@@ -44,101 +40,24 @@ public class DatabaseRequester {
      * upon firing, the database is queried for new entries
      * entries are then published in form of events
      */
-    private void initialiseTimer() {
-        TimerUtil timer = new TimerUtil(5000);
+    void initialiseTimer() {
+        TimerUtil timer = new TimerUtil(1000);
         timer.startTimer(fireCounts -> {
             System.out.println("Fired, count: " + fireCounts);
-            lastRead = new Date(Utils.getLastReadTime());
-            ArrayList<Operation> convertedEvents = generateNewOperations(new Timestamp(lastRead.getTime()));
-            if (!convertedEvents.isEmpty()) {
-                try {
-                    EventProducer.produceEvents(convertedEvents);
-                    Utils.updateLastRead(System.currentTimeMillis());
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
+            long startTime = System.currentTimeMillis();
+            System.out.println("TimeStart: " + startTime);
+            ArrayList<BookExtended> convertedEvents = getNewBooks(new Timestamp(System.currentTimeMillis()), startTime);
+            System.out.println(convertedEvents.size());
+            long endTime = System.currentTimeMillis();
+            System.out.println("TimeEnd: " + endTime);
+            long duration = endTime - startTime;
+            System.out.println("duration: " + duration);
+            System.out.println("---------------------------------------------------------------------");
             // stops timer
-            if (fireCounts == -1) {
+            if (fireCounts == 1) {
                 timer.stopTimer();
             }
         });
-    }
-
-    /**
-     * generate new operations to publish
-     * @return a list of operations
-     */
-    public ArrayList<Operation> generateNewOperations(Timestamp lastRead) {
-        ArrayList<BookExtended> books = getNewBooks(lastRead);
-        ArrayList<MemberExtended> members = getNewMembers(lastRead);
-        ArrayList<IssueExtended> issues = getNewIssues(lastRead);
-        ArrayList<MailServerInfoExtended> mailServerInfo = getNewMailServerInfos(lastRead);
-        ArrayList<Operation> operations = new ArrayList<>();
-        operations.addAll(processEntities(books, ObjectType.BOOK, "id"));
-        operations.addAll(processEntities(members, ObjectType.MEMBER, "id"));
-        operations.addAll(processEntities(issues, ObjectType.ISSUE, "bookid"));
-        operations.addAll(processEntities(mailServerInfo, ObjectType.MAIL_SERVER_INFO, "server_name"));
-        return operations;
-    }
-
-    /**
-     * processes entities by converting objects to operations
-     * The type of operations is inferred by the state of the entity, meaning:
-     *
-     * if an entity has set is_deleted to true, it is a DELETE operation.
-     * after the entities is published to the kafka topic, this tool fully deletes the entity in the database
-     *
-     * if an entity has set updated_at to null, it is an INSERT operation.
-     * to avoid that this entities is processed again the next time the timer fires,
-     * this tool sets the updated_at column to CURRENT_TIMESTAMP
-     *
-     * if updated_at is a valid value that is newer than lastTimeRead and is_deleted is false,
-     * it is an UPDATE operation.
-     * UPDATE operations do not require any additional work other than processing the event
-     *
-     * @param extendeds list of entities to process
-     * @param objectType type of object
-     * @param nameOfPrimaryKey name of the primary key (used when updating or deleting the entry after processing)
-     * @return list of operations
-     */
-    private ArrayList<Operation> processEntities(ArrayList<? extends Extended> extendeds, ObjectType objectType, String nameOfPrimaryKey) {
-        ArrayList<Operation> operations = new ArrayList<>();
-        for (Extended extended : extendeds) {
-            if (extended.getIsDeleted()) {
-                operations.add(new Operation(new Date().getTime(), OperationType.DELETE, objectType, extended.getBasicType()));
-                deleteEntity(extended, objectType, nameOfPrimaryKey);
-            } else if (!extended.getIsDeleted() && extended.getUpdatedAt() == null) {
-                operations.add(new Operation(new Date().getTime(), OperationType.INSERT, objectType, extended.getBasicType()));
-                markEntityAsChanged(extended, objectType, nameOfPrimaryKey);
-            } else if (!extended.getIsDeleted() && extended.getUpdatedAt() != null) {
-                operations.add(new Operation(new Date().getTime(), OperationType.UPDATE, objectType, extended.getBasicType()));
-            }
-        }
-        return operations;
-    }
-
-    /**
-     * marks an entity as changed by updating the updated_at column
-     * @param object the object to update
-     * @param objectType the type of the object
-     * @param nameOfPrimaryKey name of the primary key
-     */
-    private void markEntityAsChanged(Extended object, ObjectType objectType, String nameOfPrimaryKey) {
-        String queryString = "Update " + objectType.name() + " SET UPDATED_AT = CURRENT_TIMESTAMP WHERE "
-                + nameOfPrimaryKey + " = '" + object.getId() + "'";
-        execAction(queryString);
-    }
-
-    /**
-     * deletes an entity
-     * @param object the object to update
-     * @param objectType the type of the object
-     * @param nameOfPrimaryKey name of the primary key
-     */
-    private void deleteEntity(Extended object, ObjectType objectType, String nameOfPrimaryKey) {
-        String queryString = "DELETE FROM " +  objectType.name() + " WHERE " + nameOfPrimaryKey + " = '" + object.getId() + "'";
-        execAction(queryString);
     }
 
     /**
@@ -146,8 +65,10 @@ public class DatabaseRequester {
      * filters them by updated_at column
      * @return returns a list of all books that have not yet been processed
      */
-    private ArrayList<BookExtended> getNewBooks(Timestamp lastRead) {
-        ResultSet rs = execQuery("SELECT * FROM BOOK WHERE UPDATED_AT IS NULL OR UPDATED_AT > '" + lastRead + "'");
+    private ArrayList<BookExtended> getNewBooks(Timestamp lastRead, long startTime) {
+        String query = "SELECT * FROM BOOK WHERE UPDATED_AT IS NULL OR UPDATED_AT > '" + lastRead + "'";
+        //String query = "SELECT * FROM BOOK";
+        ResultSet rs = execQuery(query);
         ArrayList<BookExtended> books = new ArrayList<>();
         try {
             while (Objects.requireNonNull(rs).next()) {
@@ -164,83 +85,33 @@ public class DatabaseRequester {
         } catch (SQLException exception) {
             exception.printStackTrace();
         }
+        long queryTime = System.currentTimeMillis();
+        System.out.println("TimeQuery: " + queryTime);
+        long duration = queryTime - startTime;
+        System.out.println("QueryDuration: " + duration);
+        //books.removeIf(book -> !updatedAtNewerThanLastRead(book.getUpdatedAt()));
         return books;
     }
 
-    /**
-     * gets all the members in the database
-     * filters them by updated_at column
-     * @return returns a list of all members that have not yet been processed
-     */
-    private ArrayList<MemberExtended> getNewMembers(Timestamp lastRead) {
-        ResultSet rs = execQuery("SELECT * FROM MEMBER WHERE UPDATED_AT IS NULL OR UPDATED_AT > '" + lastRead + "'");
-        ArrayList<MemberExtended> memberExtendeds = new ArrayList<>();
-        try {
-            while (Objects.requireNonNull(rs).next()) {
-                String id = rs.getString("ID");
-                String name = rs.getString("NAME");
-                String mobile = rs.getString("MOBILE");
-                String email = rs.getString("EMAIL");
-                String updatedAt = rs.getString("UPDATED_AT");
-                boolean isDeleted = rs.getBoolean("IS_DELETED");
-                MemberExtended memberExtended = new MemberExtended(id, name, mobile, email, updatedAt, isDeleted);
-                memberExtendeds.add(memberExtended);
-            }
-        } catch (SQLException exception) {
-            exception.printStackTrace();
-        }
-        return memberExtendeds;
-    }
+
 
     /**
-     * gets all the issues in the database
-     * filters them by updated_at column
-     * @return returns a list of all issues that have not yet been processed
+     * checks if the updated_at timestamp is newer than the last time the tool ran
+     * which means the the entry belonging the to timestamp has not yet processed
+     * @param updatedAt timestamp in String format
+     * @return true if updatedAt is newer
      */
-    private ArrayList<IssueExtended> getNewIssues(Timestamp lastRead) {
-        ResultSet rs = execQuery("SELECT * FROM ISSUE WHERE UPDATED_AT IS NULL OR UPDATED_AT > '" + lastRead + "'");
-        ArrayList<IssueExtended> issueExtendeds = new ArrayList<>();
+    private boolean updatedAtNewerThanLastRead(String updatedAt) {
+        if (updatedAt == null) return true;
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-d HH:mm:ss.SSS");
+        Date fileDate;
         try {
-            while (Objects.requireNonNull(rs).next()) {
-                String bookId = rs.getString("BOOKID");
-                String memberId = rs.getString("MEMBERID");
-                String issueTime = rs.getString("ISSUETIME");
-                long renewCount = rs.getLong("RENEW_COUNT");
-                String updatedAt = rs.getString("UPDATED_AT");
-                boolean isDeleted = rs.getBoolean("IS_DELETED");
-                IssueExtended issueExtended = new IssueExtended(memberId, bookId, issueTime, renewCount, updatedAt, isDeleted);
-                issueExtendeds.add(issueExtended);
-            }
-        } catch (SQLException exception) {
-            exception.printStackTrace();
+            fileDate = format.parse(updatedAt);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return false;
         }
-        return issueExtendeds;
-    }
-
-    /**
-     * gets all the mailServerInfos in the database
-     * filters them by updated_at column
-     * @return returns a list of all mailServerInfos that have not yet been processed
-     */
-    private ArrayList<MailServerInfoExtended> getNewMailServerInfos(Timestamp lastRead) {
-        ResultSet rs = execQuery("SELECT * FROM MAIL_SERVER_INFO WHERE UPDATED_AT IS NULL OR UPDATED_AT > '" + lastRead + "'");
-        ArrayList<MailServerInfoExtended> mailServerInfoExtendeds = new ArrayList<>();
-        try {
-            while (Objects.requireNonNull(rs).next()) {
-                String id = rs.getString("SERVER_NAME");
-                int serverPort = rs.getInt("SERVER_PORT");
-                String userEmail = rs.getString("USER_EMAIL");
-                String userPassword = rs.getString("USER_PASSWORD");
-                boolean sslEnabled = rs.getBoolean("SSL_ENABLED");
-                String updatedAt = rs.getString("UPDATED_AT");
-                boolean isDeleted = rs.getBoolean("IS_DELETED");
-                MailServerInfoExtended mailServerInfoExtended = new MailServerInfoExtended(id, serverPort, userEmail, userPassword, sslEnabled, updatedAt, isDeleted);
-                mailServerInfoExtendeds.add(mailServerInfoExtended);
-            }
-        } catch (SQLException exception) {
-            exception.printStackTrace();
-        }
-        return mailServerInfoExtendeds;
+        return fileDate.after(new Date());
     }
 
     /**
--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -9,7 +9,8 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("Program started via main method");
-        new DatabaseRequester();
+        new DatabaseRequester().initialiseTimer();
+        //DatabasePreparer.fillDatabase();
     }
 
 }
